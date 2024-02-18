b52c4668c883fec56172e72f2eeb64f22d1e7d6c
ISxaNI <jasonliu99999@yahoo.com>
2024-02-18 12:54:32 -0800

    Speed changes now

---
+++ Assets/js/WIP.js
@@ -0,0 +1,6 @@
+document.getElementById('Speed').addEventListener('click', function() {
+    balls.forEach(ball => {
+        ball.dx += 1;
+        if(ball.dx < )
+    })
+})
--- Assets/js/index.js
+++ Assets/js/index.js
@@ -59,12 +59,6 @@ function detectBallCollisions() {
 document.getElementById('Sizeup').addEventListener('click', function() {
     balls.forEach(ball => {
     ball.radius += 5; 
-    if (ball.radius + ball.x > canvas.width) {
-        ball.x = canvas.width - ball.radius;
-    }
-    if (ball.radius + ball.y > canvas.height) {
-        ball.y = canvas.height - ball.radius;
-    }
 })
 });
 
@@ -89,13 +83,47 @@ document.getElementById('Add').addEventListener('click', function() {
     balls.push(newBall);
 });
 
+document.getElementById('Speedup').addEventListener('click', function() {
+    balls.forEach(ball => {
+        ball.dx += 1;
+        if(ball.dx <=  -1) {
+            ball.dx -= 1
+        }
+        ball.dy += 1;
+        if(ball.dy <= -1) {
+            ball.dy -= 1
+        }
+    })
+})
+
+document.getElementById('Speeddown').addEventListener('click', function() {
+    balls.forEach(ball => {
+        ball.dx -= 1;
+        if(-1 <= ball.dx >= 1 ) {
+            ball.dx = 0;
+        }
+        if(ball.dx <= -1) {
+            ball.dx += 1
+        }
+        ball.dy -= 1;
+        if(-1 <= ball.dy >= 1) {
+            ball.dy = 0;
+        }
+        if(ball.dy <= -1) {
+            ball.dy += 1
+        }
+    })
+})
+
 async function colorShiftToggle() {
     isColorShifting = !isColorShifting
 
     const toggleButton = document.getElementById('Color')
 
-    if (colorShiftToggle) {
-        toggleButton.style.backgroundColor = `rgb(100,100,255)`
+    if (isColorShifting) {
+        toggleButton.style.backgroundColor = `rgb( ${Math.random() * 255}, ${Math.random() * 255}, 255)`
+    } else {
+        toggleButton.style.backgroundColor = ``
     }
 
     while (isColorShifting) {
@@ -121,7 +149,7 @@ toss()
 
 //NEXT UP ON THE LIST OF WHAT TO DO:
 
-//SETUP PROPER OPERATION USE OF COLOR AND SPEED. SETTING COLOR TO RAINBOW BECAUSE HONESTLY. 
-//THAT SEEMS EASIER. SETTING SPEED SO YOU CAN INCREASE AND DECREASE THE VELOCITY OF OBJECTS. 
-//IF I REMEMBER RIGHT, I HAVE TO SETUP A ASYNC FUNCTION FOR THE COLOR CHANGE FUNCTIONALITY.
+//SETUP PROPER OPERATION USE OF  SPEED.
+//SETTING SPEED SO YOU CAN INCREASE AND DECREASE THE VELOCITY OF OBJECTS. 
+
 //DONT FORGET ABOUT THE AWAIT AND EVERYTHING TOO. NEED TO SET IT UP CORRECTLY.
